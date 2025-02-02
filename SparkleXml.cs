50efbc12f56a84408ebbc23a7685d7e25715e515
Hedgeturd <supercooper_32.1@hotmail.com>
2025-02-02 23:36:17 +0000

    Partial Part-Bin Support

--- SparkleXml.cs
+++ SparkleXml.cs
@@ -10,18 +10,19 @@ public class SparkleXml
     
     public static void WriteXml(string path)
     {
-        File.Delete(Path.Combine(Path.GetFileNameWithoutExtension(path) + ".xml"));
+        File.Delete(Path.Combine(Path.GetFileNameWithoutExtension(path) + ".p-material"));
         var xmlWriterSettings = new XmlWriterSettings { Indent = true };
-        using var writer = XmlWriter.Create(Path.GetDirectoryName(path) + "\\" + Path.GetFileNameWithoutExtension(path) + ".xml", xmlWriterSettings);
+        using var writer = XmlWriter.Create(Path.GetDirectoryName(path) + "\\" + Path.GetFileNameWithoutExtension(path) + ".p-material", xmlWriterSettings);
         
         writer.WriteStartDocument();
 
-        switch (SparkleBin.SparkleFile.Header.type)
+        switch (SparkleBin.SparkleFile.Header.Type)
         {
             case "CEffect":
+                //SparkleFunctions.CEffectWriteXml(writer, SparkleBin.SparkleFile.CEffect);
                 break;
             case "Material":
-                SparkleFunctions.MaterialWriteXML(writer, SparkleBin.SparkleFile.Material);
+                SparkleFunctions.MaterialWriteXml(writer, SparkleBin.SparkleFile.Material);
                 break;
             default:
                 Console.WriteLine("Unknown Type");
