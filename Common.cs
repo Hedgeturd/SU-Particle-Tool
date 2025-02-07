576cbfce0dd5c3a152394e040eecf1dfc13331f2
Hedgeturd <supercooper_32.1@hotmail.com>
2025-02-07 17:47:38 +0000

    Removed Unused Methods

--- Common.cs
+++ Common.cs
@@ -3,41 +3,8 @@ using System.Xml;
 
 namespace SU_Particle_Tool {
     class Common {
-        public static void WritePosition(BinaryWriter binaryWriter)
-        {
-            //Console.WriteLine(binaryWriter.BaseStream.Position - 16)
-            int pos = ((int)binaryWriter.BaseStream.Position + 4) - 24;
-            binaryWriter.Write(Common.EndianSwap(pos));
-        }
-        
-        public static void WriteNPCOffset(BinaryWriter binaryWriter, int[] ParamOffsets, int Index)
-        {
-            int currentPos = (int)binaryWriter.BaseStream.Position;
-            //Console.WriteLine(currentPos - 24);
 
-            binaryWriter.BaseStream.Position = ParamOffsets[Index];
-            //Console.WriteLine(binaryWriter.BaseStream.Position);
-            
-            binaryWriter.Write(EndianSwap(currentPos - 24));
-            
-            binaryWriter.BaseStream.Position = currentPos;
-        }
-
-        public static void WriteNPCParamOffset(BinaryWriter binaryWriter, int[] ParamOffsets, int Index)
-        {
-            //XML.NPCOffsets.Add((int)binaryWriter.BaseStream.Position);
-            int currentPos = (int)binaryWriter.BaseStream.Position;
-            //Console.WriteLine(currentPos - 24);
-
-            binaryWriter.BaseStream.Position = ParamOffsets[Index];
-            //Console.WriteLine(binaryWriter.BaseStream.Position);
-            
-            binaryWriter.Write(EndianSwap(currentPos - 24));
-            
-            binaryWriter.BaseStream.Position = currentPos;
-        }
-
-        public static int EndianSwap(int a) {
+        /*public static int EndianSwap(int a) {
             byte[] x = BitConverter.GetBytes(a);
             Array.Reverse(x);
             int b = BitConverter.ToInt32(x, 0);
@@ -49,30 +16,13 @@ namespace SU_Particle_Tool {
             Array.Reverse(x);
             float b = BitConverter.ToSingle(x, 0);
             return b;
-        }
-
-        /*public static void WriteString(BinaryWriter binaryWriter, string name)
-        {
-            XML.NPCOffsets.Add((int)binaryWriter.BaseStream.Position);
-            Common.WritePosition(binaryWriter);
-            binaryWriter.Write(Common.EndianSwap(1));
-            binaryWriter.Write(Common.EndianSwap(name.Length));
-            XML.NPCOffsets.Add((int)binaryWriter.BaseStream.Position);
-            WritePosition(binaryWriter);
-            Common.ConvString(binaryWriter, name);
-            Common.WritePadding(name, binaryWriter);
-        }
-        
-        public static void WriteFloat(BinaryWriter binaryWriter, float data)
-        {
-            XML.NPCOffsets.Add((int)binaryWriter.BaseStream.Position);
-            Common.WritePosition(binaryWriter);
-            binaryWriter.Write(Common.EndianSwap(1));
-            binaryWriter.Write(Common.EndianSwap(1));
-            XML.NPCOffsets.Add((int)binaryWriter.BaseStream.Position);
-            Common.WritePosition(binaryWriter);
-            binaryWriter.Write(Common.EndianSwapFloat(data));
         }*/
+        
+        public static void SkipPadding(int NameLength, BinaryReader binaryReader)
+        {
+            long currentPos = binaryReader.BaseStream.Position;
+            binaryReader.BaseStream.Position = (currentPos + (4 - 1)) & ~(4 - 1);
+        }
 
         public static string ReadName(BinaryReader binaryReader)
         {
@@ -87,12 +37,6 @@ namespace SU_Particle_Tool {
             bool Value = BitConverter.ToBoolean(binaryReader.ReadBytes(1), 0);
             binaryReader.BaseStream.Seek(3, SeekOrigin.Current);
             return Value;
-        }
-
-        public static void SkipPadding(int NameLength, BinaryReader binaryReader)
-        {
-            long currentPos = binaryReader.BaseStream.Position;
-            binaryReader.BaseStream.Position = (currentPos + (4 - 1)) & ~(4 - 1);
         }
 
         public static void WritePadding(string Name, BinaryWriter binaryWriter)
